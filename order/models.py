cdf42080178614c3dd62764387ef9015ee3b34ce
COLD-Electric <coldelecgtric@gmail.com>
2023-09-18 11:47:40 +0800

    feat: add product for order

--- order/models.py
+++ order/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Clients(models.Model):
     client_id = models.TextField(unique=True)
@@ -14,6 +12,7 @@ class Clients(models.Model):
     listed = models.BooleanField(null=False, default=False)
 
     class Meta:
+        managed = False
         db_table = "clients"
 
 
@@ -23,6 +22,7 @@ class SalesPeople(models.Model):
     grade = models.TextField()
 
     class Meta:
+        managed = False
         db_table = "salepeople"
 
 
@@ -32,23 +32,49 @@ class Pcs(models.Model):
     price = models.IntegerField()
 
     class Meta:
+        managed = False
         db_table = "pcs"
+
+
+class Products(models.Model):
+    product_id = models.TextField(unique=True)
+    name = models.TextField()
+    price = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = "products"
 
 
 class Orders(models.Model):
     order_id = models.TextField(unique=True)
     date = models.DateField()
-    client_id = models.ForeignKey(Clients, on_delete=models.PROTECT)
-    saleperson_id = models.ForeignKey(SalesPeople, on_delete=models.PROTECT)
+    client_id = models.ForeignKey(Clients, models.PROTECT)
+    saleperson_id = models.ForeignKey(SalesPeople, models.PROTECT)
     deliver_address = models.TextField()
     contact = models.TextField()
     order_type = models.TextField()
     oder_amount = models.IntegerField()
-    pcs_id = models.ForeignKey(Pcs, on_delete=models.PROTECT)
+    pcs_id = models.ForeignKey(Pcs, models.PROTECT)
     pcs_amount = models.IntegerField(null=False)
     batery_module_amount = models.IntegerField()
-    notice = models.TextField(null=True, blank=True)
-    note = models.TextField(null=True, blank=True)
+    notice = models.TextField(blank=True, null=True)
+    note = models.TextField(blank=True, null=True)
+    cancel = models.BooleanField()
 
     class Meta:
+        managed = False
         db_table = "orders"
+
+
+class ProductOrders(models.Model):
+    product_order_id = models.TextField(unique=True)
+    product_id = models.ForeignKey(Products, models.PROTECT)
+    amount = models.IntegerField()
+    total_price = models.IntegerField()
+    order_id = models.ForeignKey(Orders, models.PROTECT)
+    cancel = models.BooleanField()
+
+    class Meta:
+        managed = False
+        db_table = "product_orders"
